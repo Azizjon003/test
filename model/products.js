a3e866fceb936015ac5e479c029f6ca06b28e183
Azizjon <94430020+Azizjon003@users.noreply.github.com>
2023-04-26 09:59:04 +0500

    Projectga kerakli narsalar qo'shildi

--- model/products.js
+++ model/products.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      unique: true,
+      required: true,
+    },
   },
-  phone: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 const Phone = mongoose.model("Phones", productSchema);
 module.exports = Phone;
